139dbbbf2bb46cc186168fcb4d22162ae41ccab2
RafaQuant <rafa.bordafx@gmail.com>
2024-06-24 23:23:02 -0300

    contrib. period loop and result by rate were added

--- compound_interest.py
+++ compound_interest.py
@@ -8,7 +8,13 @@ def init_calculator():
     initial_investment = input("Initial investment: ")
 
     print("2. Contribution---")
+    periods = ["Anual", "Semestral", "Trimestral", "Mensual"]
     contribution_period = input("Contribución (Anual-Semestral-Trimestral-Mensual): ")
+    while contribution_period.capitalize() not in periods:
+        contribution_period = input(
+            "Por favor, ingrese el plazo tal como se lee: "
+            )
+    
     monthly_contribution = input("Contribution amount: ")
     years_contribution_qty = input("Time in years: ")
 
@@ -16,9 +22,9 @@ def init_calculator():
     estimated_interest_rate = input("Interest rate -estimated-: ")
     interest_rate_variance = input("Interest rate variance range: ")
 
-    print("4. Capitalization---")
-    capitalization_frequency = input("Capitalization frequency: ")
-
+    print("4. Capitalization---(Annual by default)")
+    #capitalization_frequency = input("Capitalization frequency: ")
+    capitalization_frequency = 1
 
 
     compound_interest_data = {
@@ -29,51 +35,13 @@ def init_calculator():
                     "years_contribution_qty": int(years_contribution_qty)
                     },                        
             "interest_rate": float(estimated_interest_rate)/100,
+            "interest_rate_var": float(interest_rate_variance)/100,
             "capitalization": int(capitalization_frequency),
             "final_capital": float(0) 
         }
     
     return compound_interest_data
 
-# def compound_interest_calc(ci_data):
-    
-#     #Para hacer más legible la fórmula
-#     init_c = ci_data['initial_investment'] #Initial capital
-#     contribution_period = ci_data['contribution']['contribution_period']
-#     mc = ci_data['contribution']['contribution_qty'] #Monthly contribution   
-#     years = ci_data['contribution']['years_contribution_qty']
-#     irate = ci_data['interest_rate']#10% se interpreta como 0.1, 
-#                                                    #si no da un número exorbitante.
-#     i_rate_var = 0 #Interest rate variance
-#     cap_freq = ci_data['capitalization']
-    
-#     cp = 0
-    
-#     if contribution_period == "Anual" or contribution_period == "anual":
-#         cp = 1
-#     elif contribution_period == "Semestral" or contribution_period == "semestral":
-#         cp = 2
-#     elif contribution_period == "Trimestral" or contribution_period == "trimestral":
-#         cp = 4
-#     elif contribution_period == "Mensual" or contribution_period == "mensual":
-#         cp = 12
-    
-    
-#     #Asignamos fórmulas a variables para que sea más legible
-#     ci_formula = init_c * (1 + irate) ** years
-#     ci_f_w_capitalization = init_c * (1 + irate/cap_freq)**(cap_freq*years)
-#     ci_f_w_add_contrib = cp * mc*(
-#         (
-#             (1+irate/cap_freq)**(cap_freq*years) - 1) / (irate/cap_freq)
-#         ) 
-    
-#     final_capital = ci_f_w_capitalization + ci_f_w_add_contrib
-                     
-#     ci_data['final_capital'] = round(final_capital, 2)
-    
-#     print(ci_data)
-    
-#     return ci_data
 
 def compound_interest_calc(calculation_result, calc_type): 
     init_c = calculation_result['initial_investment'] #Initial capital
@@ -81,7 +49,7 @@ def compound_interest_calc(calculation_result, calc_type):
     mc = calculation_result['contribution']['contribution_qty'] #Monthly contribution   
     years = calculation_result['contribution']['years_contribution_qty']
     irate = calculation_result['interest_rate']
-    i_rate_var = 0 #Interest rate variance
+    i_rate_var = calculation_result['interest_rate_var'] #Interest rate variance
     cap_freq = calculation_result['capitalization']
     
     cp = 0
@@ -95,28 +63,43 @@ def compound_interest_calc(calculation_result, calc_type):
     elif contribution_period == "Mensual" or contribution_period == "mensual":
         cp = 12
         
-    final_annual_capital = {}
+        
+        
+    min_irate = irate - i_rate_var
+    max_irate = irate + i_rate_var 
     
-    years_range = range(years + 1)
-    for n in years_range:
-        if calc_type == "basic":
-            ci_formula = init_c * (1 + irate) ** years
-        elif calc_type != "basic":
-            ci_f_w_capitalization = init_c * (1 + irate/cap_freq)**(cap_freq*n)
-            ci_f_w_add_contrib = cp * mc*(
-                 (
-                     (1+irate/cap_freq)**(cap_freq*n) - 1) / (irate/cap_freq)
-                 )#Si no hay contribuciones mensuales, todo va a dar 0. Con lo
-                  # cual, en caso de ser así, el resultado final será solo con
-                  # capitalización.
-             
-            final_capital = ci_f_w_capitalization + ci_f_w_add_contrib
-            final_capital = round(final_capital, 2)
-            
-            final_annual_capital[f'{n} Year'] = final_capital           
-            
-            
-    return final_annual_capital
+    irate_vars = [min_irate, irate, max_irate]
+    final_annual_capital_by_rate = {}
+    
+    for rate in irate_vars:
+        years_range = range(years + 1)
+        final_annual_capital = {} #Tengo que actualizar esta variable si o sí.
+                                  # si no me sobreescribre los datos  
+        for n in years_range:
+            if calc_type == "basic":
+                ci_formula = init_c * (1 + rate) ** years
+            elif calc_type != "basic":
+                ci_f_w_capitalization = init_c * (1 + rate/cap_freq)**(cap_freq*n)
+                ci_f_w_add_contrib = cp * mc*(
+                     (
+                         (1+rate/cap_freq)**(cap_freq*n) - 1) / (rate/cap_freq)
+                     )#Si no hay contribuciones mensuales, todo va a dar 0. Con lo
+                      # cual, en caso de ser así, el resultado final será solo con
+                      # capitalización.
+                 
+                final_capital = ci_f_w_capitalization + ci_f_w_add_contrib
+                final_capital = round(final_capital, 2)
+                
+                #Si no actualizo el dict arriba, esta variable es la que por
+                #   los indices, me hace sobreescribir los datos
+                final_annual_capital[f'{n} Year'] = final_capital
+        
+        
+        final_annual_capital_by_rate[round(rate, 2)] = final_annual_capital     
+                        
+    return final_annual_capital_by_rate
+
+
 
 def growth_plot(calculation_result):
     # years = range(len(calculation_result))
@@ -146,9 +129,14 @@ def variance_range_calc():
 
 data_calculation = init_calculator() #Inicia calculadora, falta el resultado.
 calculation_result = compound_interest_calc(data_calculation, "") #Dict con result.
-length = len(calculation_result) - 1
-print(calculation_result[f"{length} Year"])
-growth_plot(calculation_result)
+
+
+
+print("\nResult by rate variance: ")
+for index, irate in enumerate(calculation_result):
+    last = len(calculation_result[irate]) - 1
+    print(irate*100,"%", "--> ", calculation_result[irate][f'{last} Year'])
+    
 
 
 
